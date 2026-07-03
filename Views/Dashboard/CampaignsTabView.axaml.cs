06cf5af45744d588d30be6b38d105d4e62649579
TheBlok <tomasznosal.mail@proton.me>
2026-07-03 19:26:02 +0200

    a

---
+++ Views/Dashboard/CampaignsTabView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace DungeonApp.Views.Dashboard;
+
+public partial class CampaignsTabView : UserControl
+{
+    public CampaignsTabView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Views/Dashboard/CharactersTabView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace DungeonApp.Views.Dashboard;
+
+public partial class CharactersTabView : UserControl
+{
+    public CharactersTabView()
+    {
+        InitializeComponent();
+    }
+}
