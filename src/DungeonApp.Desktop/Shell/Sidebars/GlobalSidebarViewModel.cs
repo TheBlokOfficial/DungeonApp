4d7967fdc7fbbe01a103ddbfa206b935d733380a
TheBlok <tomasznosal.mail@proton.me>
2026-09-06 23:53:25 +0200

    feat: move sidebar toggle into brand row

--- src/DungeonApp.Desktop/Shell/Sidebars/GlobalSidebarViewModel.cs
+++ src/DungeonApp.Desktop/Shell/Sidebars/GlobalSidebarViewModel.cs
@@ -59,6 +59,7 @@ public sealed class GlobalSidebarViewModel : ObservableObject
                 RaisePropertyChanged(nameof(SidebarToggleToolTip));
                 RaisePropertyChanged(nameof(CollapsibleTextOpacity));
                 RaisePropertyChanged(nameof(CollapsibleTextOffset));
+                RaisePropertyChanged(nameof(NavigationItemsOffset));
             }
         }
     }
@@ -70,6 +71,8 @@ public sealed class GlobalSidebarViewModel : ObservableObject
     public double CollapsibleTextOpacity => IsCollapsed ? 0 : 1;
 
     public double CollapsibleTextOffset => IsCollapsed ? -12 : 0;
+
+    public double NavigationItemsOffset => IsCollapsed ? -44 : 0;
 
     /// <summary>
     /// The campaign workspace has no global top bar. Its route back to the library therefore lives
